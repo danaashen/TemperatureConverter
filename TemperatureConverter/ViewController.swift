8af0dabdf9422ee1817fd11e18df416dea3f00bf
dakosh <230107034@sdu.edu.kz>
2026-02-05 16:04:08 +0500

    Initial Commit

---
+++ TemperatureConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TemperatureConverter
+//
+//  Created by Dana Ashen on 05.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
